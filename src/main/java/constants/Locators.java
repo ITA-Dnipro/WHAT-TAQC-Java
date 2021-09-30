148e52d6a902a4983add197723668087e3eb3da4
fresh-ash <sergei081@gmail.com>
2021-09-30 17:58:01 +0300

    Added locators for Lessons page

--- src/main/java/constants/Locators.java
+++ src/main/java/constants/Locators.java
@@ -5,11 +5,17 @@ public class Locators {
     private Locators(){}
 
     public interface CommonElements{
-        String SIDE_BAR_XPATH = "//body//span[@class='sidebar__menu-item___1MMsk']";
+        String SIDE_BAR_XPATH = "//span[@class='sidebar__menu-item___1MMsk']";
     }
 
     public interface Lessons{
         String ADD_LESSON_BUTTON_XPATH = "//button[contains(.,'Add a lesson')]";
+        String ADD_THEME_BUTTON_CSS_SELECTOR = ".btn-warning";
         String FILTER_BUTTON_XPATH = "//button[contains(.,'Filter')]";
+        String SWITCH_VIEW_BUTTONS_XPATH = "//button[@class = 'btn btn-secondary']";
+        String TABLE_HEAD_THEME_NAME_XPATH = "//span[text()='themeName']";
+        String TABLE_HEAD_LESSON_DATE_XPATH = "//span[text()='lessonDate']";
+        String TABLE_HEAD_LESSON_TIME_XPATH = "//span[text()='lessonTime']";
+        String PAGE_TITLE_XPATH = "//h2[text()='Lessons']";
     }
 }
