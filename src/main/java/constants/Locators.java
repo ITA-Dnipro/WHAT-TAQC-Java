d984477782978020483398c4c249f66b553574f5
fresh-ash <sergei081@gmail.com>
2021-10-03 01:33:11 +0300

    Added locators for Lessons chapter

--- src/main/java/constants/Locators.java
+++ src/main/java/constants/Locators.java
@@ -19,13 +19,41 @@ public class Locators {
         String PAGE_TITLE_XPATH = "//h2[text()='Lessons']";
     }
 
+    public interface AddLesson{
+        String PAGE_TITLE_XPATH = "//h3[text()='Add a Lesson']";
+        String CANCEL_BUTTON_XPATH = "//button[@data-testid='cancelBtn']";
+        String CLASS_REGISTER_BUTTON_ID = "class-register-btn";
+        String LESSON_THEME_INPUT_FIELD_ID = "inputLessonTheme";
+        String GROUP_NAME_INPUT_FIELD_ID = "inputGroupName";
+        String LESSON_TIME_INPUT_FIELD_ID = "choose-date-time";
+        String MENTOR_EMAIL_INPUT_FIELD_ID = "mentorEmail";
+        String LIST_OF_GROUPS_XPATH = "//datalist[@id='group-list']/option";
+        String LIST_OF_MENTORS_XPATH = "//datalist[@id='mentor-list']/option";
+    }
+
+    public interface EditLesson{
+        String PAGE_TITLE_XPATH = "//h3[text()='Edit a Lesson']";
+        String CANCEL_BUTTON_XPATH = "//button[@data-testid='cancelBtn']";
+    }
+
+    public interface ClassBook{
+        String LIST_OF_STUDENTS_XPATH = "//p[@class='edit-lesson__link___Hgvq4']";
+        String LIST_OF_MARKS_XPATH = "//input[@class='form-control edit-lesson__mode___3T2Dn']";
+        String LIST_OF_PRESENCE_TIP_XPATH = "//input[@class='edit-lesson__mode___3T2Dn']";
+        String SAVE_BUTTON_XPATH = "//button[@type='submit']";
+    }
+
+    public interface LessonDetails{
+        String CANCEL_BUTTON_XPATH = "//button[@type='button']";
+    }
+
     public interface Courses {
         String VIEW_TABLE_CARD_XPATH = "//button[@class='btn btn-secondary']";
         String SEARCH_FIELD_XPATH = "//input[@type='text']";
         String ADD_COURSE_BUTTON_XPATH = "//span[.='Add a course']";
         String SORT_TITLE_BUTTON_XPATH = "//span[@data-sorting-param='name']";
         String COURSES_TABLE_LIST_MENTOR_STUDENT_XPATH = "//td";
-        String COURSES_TABLE_LIST_ADMIN_SECRETAR_XPATH = "//td[not(contains(@class,'text-center'))]";
+        String COURSES_TABLE_LIST_ADMIN_SECRETARY_XPATH = "//td[not(contains(@class,'text-center'))]";
         String EDIT_BUTTON_ADMIN_SECRETARY_XPATH = "//td[@class='text-center']";
         String COURSES_PAGINATION_XPATH = "//li[@class='page-item']";
         String CHANGE_ROWS_NUMBER_XPATH = "//*[@id='change-visible-people']";
