f3fe51fa346c60cba85b6442e7f166dddd5e2f75
Oleksandr Liakh <alyah124@gmail.com>
2021-10-03 22:33:07 +0200

    Add function add student

--- src/main/java/page/students/StudentsPage.java
+++ src/main/java/page/students/StudentsPage.java
@@ -16,18 +16,22 @@ import static constants.Locators.StudentsPage.*;
 
 public class StudentsPage extends Page {
 
+    @FindBy (tagName = PAGE_TITLE_TAG_NAME)
+    protected WebElement pageName;
+    @FindBy (className = SEARCH_INPUT_BOX_CLASSNAME)
+    protected WebElement searchInputFieldBox;
+    @FindBy (id = SHOW_DISABLED_SWITCHER_ID)
+    protected WebElement showDisabledSwitcher;
+    @FindBy (xpath = ADD_STUDENT_BUTTON_XPATH)
+    protected WebElement addStudentButton;
+    @FindBy (xpath = TABLE_STUDENTS_XPATH)
+    protected WebElement tableStudents;
     @FindBy (xpath = TABLE_HEAD_FIRST_NAME_XPATH)
     protected WebElement tableHeadFirstName;
     @FindBy (xpath = TABLE_HEAD_LAST_NAME_XPATH)
     protected WebElement tableHeadLastName;
     @FindBy (xpath = TABLE_HEAD_EMAIL_XPATH)
     protected WebElement tableHeadEmail;
-    @FindBy (className = SEARCH_INPUT_BOX_CLASSNAME)
-    protected WebElement searchInputFieldBox;
-    @FindBy (tagName = PAGE_TITLE_TAG_NAME)
-    protected WebElement pageName;
-    @FindBy (xpath = TABLE_STUDENTS_XPATH)
-    protected WebElement tableStudents;
 
     public StudentsPage(WebDriver driver){
         super(driver);
@@ -45,6 +49,13 @@ public class StudentsPage extends Page {
                         .collect(Collectors.toList()))
                 .collect(Collectors.toList());
         return list;
+    }
+
+    public void addStudent(){
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions
+                        .elementToBeClickable(addStudentButton));
+        clickElement(addStudentButton);
     }
 
 }
