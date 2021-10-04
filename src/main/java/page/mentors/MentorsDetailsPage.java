efc339c0e4531f70bb6115b48aceff1ce27c08e7
KharaHL <kharaherman@gmail.com>
2021-10-04 15:19:13 +0300

    write PAge object for Mentrors Details

--- src/main/java/page/mentors/MentorsDetailsPage.java
+++ src/main/java/page/mentors/MentorsDetailsPage.java
@@ -1,4 +1,31 @@
 package page.mentors;
 
-public class MentorsDatailsPage {
+import constants.Locators;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import page.base.Page;
+
+public class MentorsDetailsPage extends Page implements Locators.MentorsDetailsPage {
+    public MentorsDetailsPage(WebDriver driver) {
+        super(driver);
+    }
+    @FindBy(xpath = MENTORS_DETAILS_TAB_XPATH)
+    WebElement mentorsDetailsTab;
+    @FindBy (xpath =EDIT_MENTOR_TAB_XPATH)
+    WebElement editMentorsTab;
+    @FindBy (xpath =ARROW_BUTTON_XPATH)
+    WebElement arrowButton;
+    public MentorsTablePage backToMentorsTable() {
+        clickElement(arrowButton);
+        return new MentorsTablePage(driver);
+    }
+    public EditMentorsDetailsPage viewEditMentorsTab() {
+        clickElement(editMentorsTab);
+        return new EditMentorsDetailsPage(driver);
+    }
+    public MentorsDetailsPage viewMentorsDetailTab() {
+        clickElement(mentorsDetailsTab);
+        return this;
+    }
 }
