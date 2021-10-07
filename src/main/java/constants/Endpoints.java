80736598500352b05cb6f039ce5dc5db8c5ddd77
fresh-ash <sergei081@gmail.com>
2021-10-08 00:45:42 +0300

    Added Endpoints class

---
+++ src/main/java/constants/Endpoints.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class Endpoints {
+
+    private Endpoints() {}
+
+    public static final String BASE_URL = "http://localhost:8080";
+    public static final String LESSONS = BASE_URL + "/lessons";
+    public static final String ADD_LESSON = LESSONS + "/add";
+}
