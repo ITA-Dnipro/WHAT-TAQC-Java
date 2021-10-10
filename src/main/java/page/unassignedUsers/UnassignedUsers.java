8711ee8a47fb25d7c4d59982f7babc4e2e17f1db
Oleksandr Liakh <alyah124@gmail.com>
2021-10-11 00:48:21 +0200

    Add class UnassignedUsers

---
+++ src/main/java/page/unassignedUsers/UnassignedUsers.java
@@ -0,0 +1,11 @@
+package page.unassignedUsers;
+
+import org.openqa.selenium.WebDriver;
+import page.base.BasePage;
+
+public class UnassignedUsers extends BasePage {
+
+    public UnassignedUsers(WebDriver driver) {
+        super(driver);
+    }
+}
