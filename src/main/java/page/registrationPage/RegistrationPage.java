c3f696caf3f7b9125717e71b7f84f3469f152352
Oleksandr Liakh <alyah124@gmail.com>
2021-10-11 05:41:56 +0200

    Refactor RegistrationPage and UnassigmentUsersPageClasses

--- src/main/java/page/registrationPage/RegistrationPage.java
+++ src/main/java/page/registrationPage/RegistrationPage.java
@@ -3,8 +3,13 @@ package page.registrationPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import page.base.BasePage;
 import page.base.LogIn;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static constants.Locators.RegistrationPage.*;
 
@@ -24,23 +29,40 @@ public class RegistrationPage extends BasePage {
     WebElement singUpButton;
     @FindBy(xpath = LOG_IN_XPATH)
     WebElement logInLinkedText;
+    @FindBy(xpath = MODAL_WINDOW)
+    WebElement modalWindow;
+    @FindBy(xpath = MODAL_WINDOW_BUTTON)
+    WebElement modalWindowBackButton;
 
     public RegistrationPage(WebDriver driver) {
         super(driver);
     }
 
-    public LogIn registerUser(UserRegistration user) {
+    public LogIn registerUser(UserRegistration user, WebDriverWait waiter) {
         fillField(firstNameInputField, user.getFirstName());
         fillField(lastNameInputField, user.getLastName());
         fillField(emailInputField, user.getEmail());
         fillField(passwordInputField, user.getPassword());
         fillField(confirmPasswordInputField, user.getPassword());
         clickElement(singUpButton);
+        waiter.until(ExpectedConditions.visibilityOf(modalWindow));
+        clickElement(modalWindowBackButton);
         return new LogIn(driver);
     }
 
     public LogIn returnToLogInPage() {
         clickElement(logInLinkedText);
         return new LogIn(driver);
+    }
+
+    public List<WebElement> getAllElements(){
+        List <WebElement> listElements = new ArrayList<>();
+        listElements.add(firstNameInputField);
+        listElements.add(lastNameInputField);
+        listElements.add(emailInputField);
+        listElements.add(passwordInputField);
+        listElements.add(confirmPasswordInputField);
+        listElements.add(singUpButton);
+        return listElements;
     }
 }
--- src/main/java/page/unassignedUsers/UnassignedUsers.java
+++ src/main/java/page/unassignedUsers/UnassignedUsers.java
@@ -1,11 +1,81 @@
 package page.unassignedUsers;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import page.base.BasePage;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static constants.Locators.UnassignedUsers.*;
+
 public class UnassignedUsers extends BasePage {
+    @FindBy (xpath = UNASSIGNED_USERS_SEARCH_INPUT_FIELD_XPATH)
+    protected WebElement searchInputField;
+    @FindBy (xpath = UNASSIGNED_USERS_TABLE_HEAD_NAME_XPATH )
+    protected WebElement tableHeadFirstName;
+    @FindBy (xpath = UNASSIGNED_USERS_TABLE_HEAD_SURNAME_XPATH)
+    protected WebElement tableHeadLastName;
+    @FindBy (xpath = UNASSIGNED_USERS_TABLE_HEAD_EMAIL_XPATH)
+    protected WebElement tableHeadEmail;
+    @FindBy (xpath = UNASSIGNED_USERS_ROLE_DROPDOWN_MENU_XPATH)
+    protected WebElement roleDropDownMenu;
+    @FindBy (xpath = UNASSIGNED_USERS_STUDENT_ROLE_XPATH)
+    protected WebElement studentRole;
+    @FindBy (xpath = UNASSIGNED_USERS_MENTOR_ROLE_XPATH)
+    protected WebElement mentorRole;
+    @FindBy (xpath = UNASSIGNED_USERS_SECRETARY_ROLE_XPATH)
+    protected WebElement secretaryRole;
+    @FindBy (xpath = UNASSIGNED_USERS_ADD_ROLE_BUTTON_XPATH)
+    protected WebElement addRoleButton;
+    @FindBy(xpath = TABLE_UNASSIGNED_USERS_XPATH)
+    protected WebElement tableUnassignedUsers;
+    @FindBy (tagName = TABLE_ROW_TAG_NAME)
+    protected List <WebElement> tableUnassignedUsersRows;
+
 
     public UnassignedUsers(WebDriver driver) {
         super(driver);
+    }
+
+    public UnassignedUsers addStudentRole (String email, WebDriverWait waiter){
+        waiter.until(ExpectedConditions
+                .visibilityOfNestedElementsLocatedBy(tableUnassignedUsers, By.tagName(TABLE_ROW_TAG_NAME)));
+        WebElement row = findUserByEmail(email);
+        clickElement(row.findElement(By.xpath(UNASSIGNED_USERS_ROLE_DROPDOWN_MENU_XPATH)));
+        waiter.until(ExpectedConditions.visibilityOf(studentRole));
+        clickElement(studentRole);
+        clickElement(row.findElement(By.xpath(UNASSIGNED_USERS_ADD_ROLE_BUTTON_XPATH)));
+        return this;
+    }
+    public UnassignedUsers addMentorRole (){
+        clickElement(roleDropDownMenu);
+        clickElement(mentorRole);
+        clickElement(addRoleButton);
+        return this;
+    }
+    public UnassignedUsers addSecretaryRole (){
+        clickElement(roleDropDownMenu);
+        clickElement(mentorRole);
+        clickElement(addRoleButton);
+        return this;
+    }
+
+    public WebElement findUserByEmail(String email){
+        WebElement rowUser = tableUnassignedUsersRows.stream()
+                .filter(row -> {
+                    List<WebElement> listCells = row.findElements(By.tagName(TABLE_CELL_TAG_NAME))
+                            .stream()
+                            .limit(3)
+                            .filter(cellValue-> cellValue.getText().equals(email))
+                            .collect(Collectors.toList());
+                    return !listCells.isEmpty();
+                })
+                .findFirst().orElse(null);
+        return rowUser;
     }
 }
