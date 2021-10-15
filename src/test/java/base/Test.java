3cfefd18526b6fb2fb939ffe6cbf429ec04b7fbc
fresh-ash <sergei081@gmail.com>
2021-10-15 06:05:59 +0300

    Great mood

--- src/test/java/base/Test.java
+++ src/test/java/base/Test.java
@@ -2,34 +2,22 @@ package base;
 
 
 import constants.Endpoints;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.DataProvider;
 import page.StudentsPage;
 import page.base.Auth;
-import page.base.Page;
-import page.lessons.LessonsPage;
 import util.Role;
-
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 public class Test extends BaseTest{
 
-    @DataProvider(name = "creds")
-    public Object[][] provideCreds(){
-        return new Object[][]{{Role.ADMIN, StudentsPage.class}, {Role.MENTOR, LessonsPage.class}};
+    public Test() throws IOException {
+
     }
 
 
-    @org.testng.annotations.Test(dataProvider = "creds")
-    public void test(Role role, Class<Page> defaultPage) throws IOException {
+    @org.testng.annotations.Test
+    public void test() throws IOException {
         driver.get(Endpoints.BASE_URL);
         Auth.init(driver)
-                .logInAs(role, defaultPage);
-    }
-
-    @AfterMethod
-    public void logOut(){
-
+                .logInAs(Role.ADMIN, StudentsPage.class);
     }
 }
