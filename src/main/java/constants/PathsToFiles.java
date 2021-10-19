0dd3c71347961586313f4a4a214c06a72b87b550
fresh-ash <sergei081@gmail.com>
2021-10-19 15:27:01 +0300

    Deleted cont CREDENTIALS

--- src/main/java/constants/PathsToFiles.java
+++ src/main/java/constants/PathsToFiles.java
@@ -15,7 +15,6 @@ public class PathsToFiles {
     }
 
     public static final String BASE_PATH = "./src/main/resources/";
-    public static final String CREDENTIALS = BASE_PATH + "credentials/credentials.json";
 
     public interface Lessons {
         String ADD_LESSON_CORRECT_DATA = BASE_PATH + "lessons/AddLessonIncorrectData.json";
