b6bc497c82e58efca7cd8a54fae1ebeba9cb01b1
Victoria1991 <viktoria.poshta@gmail.com>
2021-10-19 20:37:03 +0300

    'Help1'

---
+++ src/test/java/secretarys/data/SecretarysName.java
@@ -0,0 +1,22 @@
+package secretarys.data;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+
+public class SecretarysName {
+
+    private String secretarysName;
+
+    public String getName() {
+        return secretarysName;
+    }
+
+    public void setName(String name) {
+        this.secretarysName = name;
+    }
+
+    public static SecretarysName[] getData(String filename) throws IOException {
+        return new ObjectMapper().readValue(new File(filename), SecretarysName[].class);
+}
+}
