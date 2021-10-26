02aeddab175e5ec69ccab5ba75e38907bf4e069e
KharaHL <kharaherman@gmail.com>
2021-10-26 16:35:57 +0300

    add entities for mentor

---
+++ src/main/java/api/entities/mentors/ActiveMentors.java
@@ -0,0 +1,13 @@
+package api.entities.mentors;
+
+public class ActiveMentors extends MentorInformation {
+    String avatarUrl;
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+}
---
+++ src/main/java/api/entities/mentors/MentorInformation.java
@@ -0,0 +1,40 @@
+package api.entities.mentors;
+
+public class MentorInformation {
+    Integer id;
+    String email;
+    String firstName;
+    String lastName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
