04889c0a005a2d820b38c819e4abb189c808436e
Hetman <gggomelll@gmail.com>
2021-10-26 14:34:37 +0300

    Add Courses endpoints

--- src/main/java/api/APIConstants.java
+++ src/main/java/api/APIConstants.java
@@ -1,11 +1,17 @@
 package api;
 
+import org.apache.http.conn.util.PublicSuffixList;
+
 public class APIConstants {
     public static final String BASE_LINK = "https://charliebackendapi.azurewebsites.net/api/v2";
 
     public interface AccountEndpoints {
-        String COURSES_ENDPOINT = BASE_LINK + "/courses";
         String AUTH_ENDPOINT = BASE_LINK + "/accounts/auth";
+        String CHANGE_PASSWORD = BASE_LINK + "/api/v2/accounts/password";
+    }
+
+    public interface Courses {
+        String COURSES_ENDPOINT = BASE_LINK + "/courses";
     }
 
     public interface Headers {
