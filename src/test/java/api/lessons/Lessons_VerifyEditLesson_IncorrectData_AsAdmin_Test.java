8dd8aab8c14dc0692099c8dbe93f04c8381483f0
fresh-ash <sergei081@gmail.com>
2021-10-31 00:24:42 +0300

    Added test for EditLesson incorrect data

--- src/test/java/api/lessons/Lessons_VerifyEditLesson_IncorrectData_AsAdmin_Test.java
+++ src/test/java/api/lessons/Lessons_VerifyEditLesson_IncorrectData_AsAdmin_Test.java
@@ -2,12 +2,13 @@ package api.lessons;
 
 import api.APIConstants;
 import api.base.AdminRequests;
+import api.base.BaseRequests;
 import api.entities.error.Error;
 import api.entities.error.ResponseError;
 import api.entities.lessons.Lesson;
 import api.services.LessonServiceApi;
-import org.jsoup.Connection;
-import org.testng.annotations.BeforeClass;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import util.RandomStringsGenerator;
@@ -20,51 +21,48 @@ import static api.APIConstants.*;
 import static api.APIConstants.StatusCodes.BAD_REQUEST;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class Lesson_AddLesson_IncorrectData_AsAdmin {
+public class Lessons_VerifyEditLesson_IncorrectData_AsAdmin_Test {
 
-    LessonServiceApi lessonServiceApi;
     Lesson lesson;
+    BaseRequests requests;
+    LessonServiceApi serviceApi;
     static final Integer overSizeOfThemeName = 101;
     static final String futureDate = LocalDateTime.now().plusDays(1).format(DateTimeFormatter.ofPattern(dateFormat));
 
-    @BeforeClass
+    @BeforeTest
     public void setUp() throws IOException {
-        lessonServiceApi = new LessonServiceApi(new AdminRequests());
-        lesson = Lesson.getLessonObject();
+        requests = new AdminRequests();
+        serviceApi = new LessonServiceApi(requests);
+        lesson = serviceApi.addLesson(Lesson.getLessonObject()).as(Lesson.class);
     }
 
     @DataProvider(name = "lesson")
     public Object[][] provide() throws CloneNotSupportedException {
         return new Object[][]{
                 {lesson.clone().setLessonDate(futureDate),
-                new ResponseError(new Error().setMessage(APIConstants.Lessons.BAD_DATE_MESSAGE))},
+                        new ResponseError(new Error().setMessage(Lessons.BAD_DATE_MESSAGE))},
 
                 {lesson.clone().setThemeName(RandomStringsGenerator
                         .getAlphabeticStringLowerCaseCharacters(overSizeOfThemeName)),
-                            new ResponseError(new Error().setMessage(Lessons.TOO_LONG_THEME_NAME_MESSAGE))},
-
-                {lesson.clone().setStudentGroupId(0),
-                new ResponseError(new Error().setMessage(Lessons.EMPTY_STUDENT_GROUP_MESSAGE))},
-
-                {lesson.clone().setMentorId(0),
-                new ResponseError(new Error().setMessage(Lessons.EMPTY_MENTOR_ID_MESSAGE))},
+                        new ResponseError(new Error().setMessage(Lessons.TOO_LONG_THEME_NAME_MESSAGE))},
 
                 {lesson.clone().setThemeName(RandomStringsGenerator
                         .getAlphabeticStringLowerCaseCharacters(0)),
-                new ResponseError(new Error().setMessage(Lessons.EMPTY_THEME_NAME_MESSAGE))}};
+                        new ResponseError(new Error().setMessage(Lessons.EMPTY_THEME_NAME_MESSAGE))}};
     }
 
     @Test(dataProvider = "lesson")
-    public void addLessonIncorrectData(Lesson lesson, ResponseError error) throws IOException {
+    public void editLessonAsAdmin(Lesson lesson, ResponseError responseError) throws
+            JsonProcessingException {
 
-        ResponseError responseError = lessonServiceApi.addLesson(lesson)
+        ResponseError error = serviceApi.addLesson(lesson)
                 .then()
                 .log().all()
                 .statusCode(BAD_REQUEST)
                 .extract()
                 .as(ResponseError.class);
 
-        assertThat(responseError.getError().getMessage())
-                .isEqualTo(error.getError().getMessage());
+        assertThat(error.getError().getMessage())
+                .isEqualTo(responseError.getError().getMessage());
     }
 }
