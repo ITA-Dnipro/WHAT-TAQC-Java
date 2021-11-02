ef4941ade55d8199f6bdd27061470a438c8eddfa
KharaHL <kharaherman@gmail.com>
2021-11-02 17:46:11 +0200

    write new asserts

--- src/test/java/mentors/MentorsTablePage_VerifySortingOfDisableMentors_AdminRole.java
+++ src/test/java/mentors/MentorsTablePage_VerifySortingOfDisableMentors_AdminRole.java
@@ -43,7 +43,8 @@ public class MentorsTablePage_VerifySortingOfDisableMentors_AdminRole extends Ba
                 .sortByEmail()
                 .verifySoftByEmailASC()
                 .sortByEmail()
-                .verifySoftByEmailDEC();
+                .verifySoftByEmailDEC()
+                .assertAll();
     }
 
 }
