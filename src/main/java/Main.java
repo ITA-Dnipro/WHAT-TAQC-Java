cc7cef6e1db3fefcdcb2594c2637ae72d6d7f359
fresh-ash <sergei081@gmail.com>
2021-11-03 14:49:05 +0200

    Deleted imports Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,12 @@
-import com.beust.jcommander.internal.Lists;
 import org.testng.TestNG;
 import org.testng.xml.SuiteXmlParser;
 import org.testng.xml.XmlSuite;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
-import java.util.Properties;
 
 
 public class Main {
